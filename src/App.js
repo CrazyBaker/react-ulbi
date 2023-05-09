c43495492fd27010b26a1a50f37a689c9fbfe738
a.zigunov <zigunovchrome@gmail.com>
2023-05-09 20:40:10 +0300

    add Postlist component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import React, { useState } from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import PostItem from "./components/PostItem";
+import PostList from './components/PostList';
 
 function App() {
-  const [value, setValue] = useState('Текст');
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Javascript', body: 'Description'},
+    {id: 2, title: 'React', body: 'Description'},
+    {id: 3, title: 'Redux', body: 'Description'},
+  ]);
 
+  const [posts2, setPosts2] = useState([
+    {id: 1, title: 'Python', body: 'Description'},
+    {id: 2, title: 'Django', body: 'Description'},
+    {id: 3, title: 'MobX', body: 'Description'},
+  ]);
 
   return (
     <div className="App">
-      <PostItem post={{id: 1, title: 'Javascript', body: 'Description'}}/>
-      <PostItem post={{id: 2, title: 'React', body: 'Description'}}/>
-      <PostItem post={{id: 3, title: 'Redux', body: 'Description'}}/>
+    <PostList posts={posts} title={'Список постов'}/>
+    <PostList posts={posts2} title={'Другой список постов'}/>
     </div>
   );
 }
