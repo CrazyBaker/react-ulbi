96dd4a71111850a38486a828dd9cfd426e26a703
a.zigunov <zigunovchrome@gmail.com>
2023-05-09 19:36:07 +0300

    add classCounter component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
 
 function App() {
   const [value, setValue] = useState('Текст');
@@ -8,14 +9,7 @@ function App() {
   return (
     <div className="App">
       <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
+      <ClassCounter/>
     </div>
   );
 }
