1fe98b22ce7a4284d78e163b059b05e28c52e110
a.zigunov <zigunovchrome@gmail.com>
2023-05-09 21:55:12 +0300

    add MyButton component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import PostItem from "./components/PostItem";
 import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -20,6 +21,11 @@ function App() {
 
   return (
     <div className="App">
+    <form>
+      <input type="text" placeholder='Название поста'/>
+      <input type="text" placeholder='Описание поста'/>
+      <MyButton>Создать пост</MyButton>
+    </form>
     <PostList posts={posts} title={'Список постов'}/>
     <PostList posts={posts2} title={'Другой список постов'}/>
     </div>
